9fae2b38ff3faab44987291dd6caa4053f2b261c
Oluwaseyi <kdom22033@gmail.com>
2024-07-17 05:18:20 +0100

    Update main.js

--- ThriveLink Hub/public/main.js
+++ ThriveLink Hub/public/main.js
@@ -1,45 +1,4 @@
-//------------------------------- Classes display for latest sermon  -------------------------------
-
-$('.live').slick({
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-
-
-  //------------------------------- Testimonial display for latest sermon  -------------------------------
+//------------------------------- Testimonial display for latest sermon  -------------------------------
 
 $('.quote').slick({
     dots: false,
